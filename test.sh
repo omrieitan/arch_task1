cd8c471460e38ecabb4607d97a1ac280bcd344de
omri <omrieitan@gmail.com>
2018-03-25 22:15:45 +0300

    fixed bug in subtract added tests for add and sub

--- test.sh
+++ test.sh
@@ -3,6 +3,28 @@
 MUN_OF_TESTS=10
 NUM_OF_TESTS_FAILED=0
 
+function bignum()
+{
+    local  num=""
+    if [ $(($RANDOM%2)) == 1 ];
+        then num="_"
+    fi
+    num+="$(( ( $RANDOM % 9 ) +1 ))"
+    for ((number=1;number < $(( ( $RANDOM % 300 )  + 1 ));number++))
+    {
+        num+="$(( ( $RANDOM % 9 ) ))"
+    }
+    echo $num
+}
+function rand_sign(){
+local  sign=""
+if [ $(($RANDOM%2)) == 1 ];
+        then sign="-"
+        else sign="+"
+    fi
+    echo $sign
+}
+
 echo -e "\e[1m_________TASK1 TEST_________\e[0m"
 echo ""
 
@@ -11,25 +33,8 @@ echo -e "\e[1mADD OPERATION_________\e[0m"
 counter=1
 while [ $counter -le $MUN_OF_TESTS ]
 do
-    NUM1=""
-    NUM2=""
-    if [ $(($RANDOM%2)) == 1 ];
-        then NUM1="_"
-    fi
-    if [ $(($RANDOM%2)) == 1 ];
-        then NUM2="_"
-    fi
-    NUM1+="$(( ( $RANDOM % 9 ) +1 ))"
-    NUM2+="$(( ( $RANDOM % 9 ) +1 ))"
-    
-    for ((number=1;number < $(( ( $RANDOM % 300 )  + 1 ));number++))
-    {
-        NUM1+="$(( ( $RANDOM % 9 ) ))"
-    }
-    for ((number=1;number < $(( ( $RANDOM % 300 )  + 1 ));number++))
-    {
-        NUM2+="$(( ( $RANDOM % 9 ) ))"
-    }
+    NUM1=$(bignum)
+    NUM2=$(bignum)
     
     echo "test$counter: $NUM1 + $NUM2 ="
     TEST="$(./bin/calc <<< $NUM1' '$NUM2' +')"
@@ -53,25 +58,8 @@ echo -e "\e[1mSUB OPERATION_________\e[0m"
 counter=1
 while [ $counter -le $MUN_OF_TESTS ]
 do
-    NUM1=""
-    NUM2=""
-    if [ $(($RANDOM%2)) == 1 ];
-        then NUM1="_"
-    fi
-    if [ $(($RANDOM%2)) == 1 ];
-        then NUM2="_"
-    fi
-    NUM1+="$(( ( $RANDOM % 9 ) +1 ))"
-    NUM2+="$(( ( $RANDOM % 9 ) +1 ))"
-    
-    for ((number=1;number < $(( ( $RANDOM % 300 )  + 1 ));number++))
-    {
-        NUM1+="$(( ( $RANDOM % 9 ) ))"
-    }
-    for ((number=1;number < $(( ( $RANDOM % 300 )  + 1 ));number++))
-    {
-        NUM2+="$(( ( $RANDOM % 9 ) ))"
-    }
+    NUM1=$(bignum)
+    NUM2=$(bignum)
     
     echo "test$counter: $NUM1 - $NUM2 ="
     TEST="$(./bin/calc <<< $NUM1' '$NUM2' -')"
@@ -81,7 +69,7 @@ do
         then
         echo -e "\e[41mFAILED\e[49m"
         echo -e "\e[41mexpected: "$CORRECT"\e[49m"
-        echo -e "\e[41mgot: "$TEST"\e[49m"
+        echo -e "\e[41m     got: "$TEST"\e[49m"
         ((NUM_OF_TESTS_FAILED++))
         else
         echo -e "\e[92mPASSED\e[39m"
@@ -89,9 +77,34 @@ do
     ((counter++))
 done
 
+echo ""
+echo -e "\e[1mADD&SUB OPERATIONS_____\e[0m"
+
+ARG="$(bignum) $(bignum) $(rand_sign)"
+for ((number=1;number < $(( ( $RANDOM % 20 )  + 1 ));number++))
+    {
+        ARG+=" $(bignum) $(rand_sign)"
+    }
+echo "test$counter: $ARG"
+    TEST="$(./bin/calc <<< $ARG)"
+    CORRECT="$(dc <<< $ARG' pq')"
+
+    if [ "$CORRECT" != "$TEST" ];
+        then
+        echo -e "\e[41mFAILED\e[49m"
+        echo -e "\e[41mexpected: "$CORRECT"\e[49m"
+        echo -e "\e[41m     got: "$TEST"\e[49m"
+        ((NUM_OF_TESTS_FAILED++))
+        else
+        echo -e "\e[92mPASSED\e[39m"
+    fi
+
+
 if [ "$NUM_OF_TESTS_FAILED" == 0 ];
     then
     echo -e "\e[42mPASSED ALL TESTS, WELL DONE =]\e[49m"
     else
     echo -e "\e[41mFailed "$NUM_OF_TESTS_FAILED" tests.\e[49m"
 fi
+
+
